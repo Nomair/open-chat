94444e5d7e4d4a82c945ed24a5fe53f0a7af7551
nomair.ghanem <nomair.ghanem@stoneage.com.br>
2021-05-23 17:45:05 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const { ApolloServer, gql } = require("apollo-server");
+const crypto = require('crypto')
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => "worldasd",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
